2089b77596525c23a1d3cb871bf9487920bf01ec
cfstras <cfstras@users.noreply.github.com>
2019-03-12 22:48:32 +0100

    better setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 set -euxo pipefail
 
-rm -rf .env
-mkdir .env
-python3 -m venv .env
-source .env/bin/activate
-pip install -r requirements.txt
+SERVICE_PATH=$HOME/.config/systemd/user/
+SERVICE=money.service
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+ENV=.env
+PIP=$ENV/bin/pip
+
+make_venv() {
+    [[ -x $PIP ]] || python3 -m venv $ENV
+    $PIP install -r requirements.txt
+}
+
+install_service() {
+    mkdir -p "$SERVICE_PATH"
+
+    sed "s#DIR#${DIR}#g" "$SERVICE" > "$SERVICE_PATH/$SERVICE"
+    loginctl enable-linger # allow our user to start permanent services
+    systemctl --user daemon-reload
+    systemctl --user enable --now "$SERVICE"
+}
+
+make_venv
+install_service
