0259a26c41bf6770c3e870f3cde6be102048d5aa
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-08-06 06:41:14 +0200

    refactor: format shell scripts

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 SERVICE_PATH=$HOME/.config/systemd/user/
 SERVICE=money.service
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 make_venv() {
     uv sync --no-install-workspace
@@ -12,7 +12,7 @@ make_venv() {
 install_service() {
     mkdir -p "$SERVICE_PATH"
 
-    sed "s#DIR#${DIR}#g" "$SERVICE" > "$SERVICE_PATH/$SERVICE"
+    sed "s#DIR#${DIR}#g" "$SERVICE" >"$SERVICE_PATH/$SERVICE"
     systemctl --user daemon-reload
     #systemctl --user enable --now "$SERVICE"
 }
